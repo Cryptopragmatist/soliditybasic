744ed3bcc27ade29b79993016e6ad704fcf0e546
Noderunner.eth <tristan.alberio@gmail.com>
2021-08-18 13:45:31 +0800

    add totalwave fn and wave fn

--- WavePortal.sol
+++ WavePortal.sol
@@ -6,8 +6,21 @@ pragma solidity ^0.8.4; // telling the sol compiler to run this version and noth
 import "hardhat/console.sol"; //importing some stuff from Hardhat
 
 contract WavePortal { //  contract is a collection of code (its functions) and data (its state) that resides at a specific address on the Ethereum blockchain
+
+    uint totalWaves; // 32-bit unsigned integer for totalWaves
     constructor() { // declaring the function of the contract
         console.log("I am the contract"); //printing function of solidity
+    }
+
+    function wave() public { // create a function called wave to count total waves, make it public so that users can see
+        totalWaves += 1; // count the total waves
+        console.log("%s waved", msg.sender); // %s sending a string to be printed //msg.sender =  wallet of function caller, authentication. need a valid wallet to call the function
+
+    }
+
+    function getTotalWaves() view public returns (uint) { // this returns an integer
+      return totalWaves; // returns total waves
+
     }
 }
 
